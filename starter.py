cf64ad6a2db2fd31cf27e72ef7eef8f8c51f5323
Aidar Darmesh <darmesh.aidar@gmail.com>
2024-10-10 16:19:02 +0500

    Llama Index: Starter lesson

---
+++ starter.py
@@ -0,0 +1,28 @@
+import os.path
+from dotenv import load_dotenv
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
+
+load_dotenv()
+
+# check if storage already exists
+PERSIST_DIR = "./storage"
+if not os.path.exists(PERSIST_DIR):
+    # load the documents and create the index
+    documents = SimpleDirectoryReader("data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+# Either way we can now query the index
+query_engine = index.as_query_engine()
+response = query_engine.query("How Paul started programming? Keep it short.")
+print(response)

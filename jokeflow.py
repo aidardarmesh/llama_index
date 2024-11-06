f99a60fed3be5d67913d117264ab54684fcb7d5d
Aidar Darmesh <darmesh.aidar@gmail.com>
2024-11-06 15:11:43 +0500

    Llama Workflow

---
+++ jokeflow.py
@@ -0,0 +1,47 @@
+from llama_index.core.workflow import (
+    Event,
+    StartEvent,
+    StopEvent,
+    Workflow,
+    step,
+)
+
+# `pip install llama-index-llms-openai` if you don't already have it
+from llama_index.llms.openai import OpenAI
+
+
+class JokeEvent(Event):
+    joke: str
+
+
+class JokeFlow(Workflow):
+    llm = OpenAI()
+
+    @step
+    async def generate_joke(self, ev: StartEvent) -> JokeEvent:
+        topic = ev.topic
+
+        prompt = f"Write your best joke about {topic}."
+        response = await self.llm.acomplete(prompt)
+        print(response)
+        return JokeEvent(joke=str(response))
+
+    @step
+    async def critique_joke(self, ev: JokeEvent) -> StopEvent:
+        joke = ev.joke
+
+        prompt = f"Give a thorough analysis and critique of the following joke: {joke}"
+        response = await self.llm.acomplete(prompt)
+        return StopEvent(result=str(response))
+
+
+async def main():
+    w = JokeFlow(timeout=60, verbose=False)
+    result = await w.run(topic="pirates")
+    print(result)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
